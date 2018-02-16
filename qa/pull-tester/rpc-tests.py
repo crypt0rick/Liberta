1c32985cc0b92b4f2bf724893c7b0f098d82f72f
crypt0rick <33953035+crypt0rick@users.noreply.github.com>
2018-02-16 12:10:30 +0200

    Initialize liberta

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (c) 2014-2016 The Kore Core developers
+# Copyright (c) 2014-2016 The Liberta Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -17,7 +17,7 @@ than:
       interface.
 
 For a description of arguments recognized by test scripts, see
-`qa/pull-tester/test_framework/test_framework.py:KoreTestFramework.main`.
+`qa/pull-tester/test_framework/test_framework.py:LibertaTestFramework.main`.
 
 """
 
@@ -34,8 +34,8 @@ from tests_config import *
 #If imported values are not defined then set to zero (or disabled)
 if 'ENABLE_WALLET' not in vars():
     ENABLE_WALLET=0
-if 'ENABLE_BITCOIND' not in vars():
-    ENABLE_BITCOIND=0
+if 'ENABLE_LIBERTAD' not in vars():
+    ENABLE_LIBERTAD=0
 if 'ENABLE_UTILS' not in vars():
     ENABLE_UTILS=0
 if 'ENABLE_ZMQ' not in vars():
@@ -62,19 +62,19 @@ for arg in sys.argv[1:]:
 
 #Set env vars
 buildDir = BUILDDIR
-if "BITCOIND" not in os.environ:
-    os.environ["BITCOIND"] = buildDir + '/src/kored' + EXEEXT
-if "BITCOINCLI" not in os.environ:
-    os.environ["BITCOINCLI"] = buildDir + '/src/kore-cli' + EXEEXT
+if "LIBERTAD" not in os.environ:
+    os.environ["LIBERTAD"] = buildDir + '/src/libertad' + EXEEXT
+if "LIBERTACLI" not in os.environ:
+    os.environ["LIBERTACLI"] = buildDir + '/src/liberta-cli' + EXEEXT
 
 if EXEEXT == ".exe" and "-win" not in opts:
-    # https://github.com/kore/kore/commit/d52802551752140cf41f0d9a225a43e84404d3e9
-    # https://github.com/kore/kore/pull/5677#issuecomment-136646964
+    # https://github.com/liberta/liberta/commit/d52802551752140cf41f0d9a225a43e84404d3e9
+    # https://github.com/liberta/liberta/pull/5677#issuecomment-136646964
     print("Win tests currently disabled by default.  Use -win option to enable")
     sys.exit(0)
 
-if not (ENABLE_WALLET == 1 and ENABLE_UTILS == 1 and ENABLE_BITCOIND == 1):
-    print("No rpc tests to run. Wallet, utils, and kored must all be enabled")
+if not (ENABLE_WALLET == 1 and ENABLE_UTILS == 1 and ENABLE_LIBERTAD == 1):
+    print("No rpc tests to run. Wallet, utils, and libertad must all be enabled")
     sys.exit(0)
 
 # python3-zmq may not be installed. Handle this gracefully and with some helpful info
@@ -208,7 +208,7 @@ class RPCCoverage(object):
     Coverage calculation works by having each test script subprocess write
     coverage files into a particular directory. These files contain the RPC
     commands invoked during testing, as well as a complete listing of RPC
-    commands per `kore-cli help` (`rpc_interface.txt`).
+    commands per `liberta-cli help` (`rpc_interface.txt`).
 
     After all tests complete, the commands run are combined and diff'd against
     the complete list to calculate uncovered RPC commands.

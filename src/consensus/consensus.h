1108df20c06667eae8501cc983818eb263d6d790
crypt0rick <33953035+crypt0rick@users.noreply.github.com>
2018-02-16 12:39:22 +0200

    Initialize Liberta

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -1,10 +1,10 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2015 The Kore Core developers
+// Copyright (c) 2009-2015 The Liberta Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_CONSENSUS_CONSENSUS_H
-#define BITCOIN_CONSENSUS_CONSENSUS_H
+#ifndef LIBERTA_CONSENSUS_CONSENSUS_H
+#define LIBERTA_CONSENSUS_CONSENSUS_H
 
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
@@ -24,4 +24,4 @@ enum {
     LOCKTIME_MEDIAN_TIME_PAST = (1 << 1),
 };
 
-#endif // BITCOIN_CONSENSUS_CONSENSUS_H
+#endif // LIBERTA_CONSENSUS_CONSENSUS_H

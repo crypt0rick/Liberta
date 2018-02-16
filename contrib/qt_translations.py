31d7faa9f371f5135889327615b8e4d0472b7e90
crypt0rick <33953035+crypt0rick@users.noreply.github.com>
2018-02-16 11:43:45 +0200

    initialize Liberta

--- contrib/qt_translations.py
+++ contrib/qt_translations.py
@@ -2,7 +2,7 @@
 
 # Helpful little script that spits out a comma-separated list of
 # language codes for Qt icons that should be included
-# in binary kore distributions
+# in binary liberta distributions
 
 import glob
 import os
@@ -10,13 +10,13 @@ import re
 import sys
 
 if len(sys.argv) != 3:
-  sys.exit("Usage: %s $QTDIR/translations $BITCOINDIR/src/qt/locale"%sys.argv[0])
+  sys.exit("Usage: %s $QTDIR/translations $LIBERTADIR/src/qt/locale"%sys.argv[0])
 
 d1 = sys.argv[1]
 d2 = sys.argv[2]
 
 l1 = set([ re.search(r'qt_(.*).qm', f).group(1) for f in glob.glob(os.path.join(d1, 'qt_*.qm')) ])
-l2 = set([ re.search(r'kore_(.*).qm', f).group(1) for f in glob.glob(os.path.join(d2, 'kore_*.qm')) ])
+l2 = set([ re.search(r'liberta_(.*).qm', f).group(1) for f in glob.glob(os.path.join(d2, 'liberta_*.qm')) ])
 
 print ",".join(sorted(l1.intersection(l2)))
 
